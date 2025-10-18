da31210b69c1ccd2fa0e56564ef56b77a97bd406
yulliatr <trofymenkoyulia@gmail.com>
2025-10-18 22:25:52 +0300

    Test failing Husky hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 function App() {
-  return <div className="App">Hello!</div>;
+  // навмисна помилка для перевірки husky
+  nonexistentFunction();
+
+  return <div className="App"> Hello from Husky! </div>;
 }
 
 export default App;
